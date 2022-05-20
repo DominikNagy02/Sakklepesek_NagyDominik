70b3f1c3539533d3ce01ba74e07d1c681c0ae4b7
DominikNagy02 <bigdodo2004@gmail.com>
2022-05-20 20:39:26 +0200

    Tábla alap

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,38 @@ using System.Windows.Shapes;
 
 namespace Sakklepesek_NagyDominik
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            TablaGeneral();
+
+        }
+
+        private void TablaGeneral()
+        {
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                Tabla.RowDefinitions.Add(new RowDefinition());
+                Tabla.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    Button y = new Button();
+                    y.Content = j + $"{i + 1}";
+                    y.Height = 30;
+                    y.Width = 30;
+                    Grid.SetColumn(y, i);
+                    Grid.SetRow(y, j);
+                    Tabla.Children.Add(y);
+
+                }
+            }
         }
     }
 }
